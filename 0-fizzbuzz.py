da4820b2c9ea44ab8a400cab252c0edbcc4e8469
zinawt22 <104650966+zinawt22@users.noreply.github.com>
2022-11-13 05:50:45 -0800

    Update 0-fizzbuzz.py

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -17,12 +17,12 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0:
+        if (i % 3) == 0: 
             tmp_result.append("Fizz")
         elif (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
-        elif (i % 5) == 0:
-            tmp_result.append("Buzz")
+        elif (i % 3) == 0:
+            #append result
         else:
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
